2f886a9f3ec2f43aac4abaa3e6e569cc50db9866
SupperSpiderMan <Andy_Zhong_Spider@163.com>
2020-03-31 15:04:33 +0800

    通用新闻爬虫

---
+++ AutoExtractCrawlSpider/crawls/crawl_auto/spiders/auto_spiders.py
@@ -0,0 +1,51 @@
+from crawl_commons.spiders.auto_spider import *
+
+
+# 单独测试使用
+class AutoSpider1(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider1"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider2(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider2"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider3(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider3"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider4(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider4"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider5(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider5"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider6(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider6"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider7(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider7"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider8(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider8"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider9(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider9"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class AutoSpider10(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "auto_spider10"  # 定义蜘蛛名
---
+++ AutoExtractCrawlSpider/crawls/crawl_test/spiders/test_error_spiders.py
@@ -0,0 +1,22 @@
+from crawl_commons.spiders.auto_spider import *
+from crawl_commons.spiders.common_spider import *
+
+
+# 单独测试使用
+class TestAutoErrorSpider(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "test_auto_error_spider"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class HistoryTestAutoErrorSpider(AutoSpider):  # 需要继承scrapy.Spider类
+    name = "history_test_auto_error_spider"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class TestErrorSpider(CommonSpider):  # 需要继承scrapy.Spider类
+    name = "test_error_spider"  # 定义蜘蛛名
+
+
+# 单独测试使用
+class HistoryTestAutoSpider(CommonSpider):  # 需要继承scrapy.Spider类
+    name = "history_test_error_spider"  # 定义蜘蛛名
